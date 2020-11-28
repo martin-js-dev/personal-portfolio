d7b256d8116a0db30cdee9f79ba62b617b508b2d
Martin Velinov <54155837+martinvelinov@users.noreply.github.com>
2020-11-28 16:04:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -69,3 +69,36 @@ function init() {
 }
 
 
+
+
+/*---------------------- banner-------------------*/
+
+const counters = document.querySelectorAll('.counter');
+const speed = 100; 
+
+counters.forEach(counter => {
+const updateCount = () => {
+const target = +counter.getAttribute('data-target');
+const count = +counter.innerText;
+
+
+const inc = target / speed;
+
+
+if (count < target) {
+
+counter.innerText = count + inc;
+
+setTimeout(updateCount, 1);
+} else {
+counter.innerText = target;
+}
+};
+
+updateCount();
+});
+
+
+
+
+
